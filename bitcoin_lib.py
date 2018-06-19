2f06c91a7b6e8f328954c5da8bd327ba422c66d3
Andrew Pfalz <apfalz@cct.lsu.edu>
2018-06-19 13:34:05 -0500

    working people, miners, exchange, network

--- bitcoin_lib.py
+++ bitcoin_lib.py
@@ -1,6 +1,7 @@
 import datetime as dt
 import time
 import numpy as np
+import threading
 from Crypto import Random
 from Crypto.PublicKey import RSA
 
@@ -9,6 +10,8 @@ class People:
         self.people   = {}
         self.exchange = exchange_instance
         self.names    = ['Alice', 'Bob', 'Carly', 'Dave', 'Emma', 'Frank', 'Gwen', 'Henry', 'Iggy', 'James', 'Karen', 'Larry']
+        self.verbose  = 2
+
         #add people to the table
         for i, name in enumerate(self.names):
             self.add_person(Person(np.random.randint(1, 20), i, name))
@@ -23,7 +26,8 @@ class People:
             amount    = np.random.randint(1, 20)
             posessed  = '/' + str(sender.coin_possessed)
             trade     = Trade(sender, recipient, amount)
-            print(sender.name + ' wants to send ' + str(amount) + posessed + ' to ' + recipient.name)
+            if self.verbose >= 2:
+                print(sender.name + ' wants to send ' + str(amount) + posessed + ' to ' + recipient.name)
             self.exchange.receive_request(trade)
 
 
@@ -32,26 +36,29 @@ class People:
 class Block:
     def __init__(self, prev_hash, transaction_list):
         self.prev_hash    = prev_hash
-        self.transactions = str(transaction_list) #really this is a merkle root of the transactions
+        self.transactions = transaction_list#Not accurate for now, really this is a merkle root of the transactions
         self.nonce        = 0
         self.timestamp    = dt.datetime.now()
-        self.target       = 3241 #number of leading zeros in winning hash
+        self.target       = 10 #for now this is the delay to wait before successfully creating block
         self.verbose      = 2 #2: print everything, 1: print normally, 0: print nothing
         self.print_freq   = 100
         self.hash         = self.create_hash()
-        self.solve_puzzle()
+        #self.solve_puzzle()
 
 
     def create_hash(self):
-        prev     = sum([ord(i) for i in self.prev_hash])
-        trans    = sum([ord(i) for i in self.transactions])
+        prev     = sum([ord(i) for i in str(self.prev_hash)])
+        trans    = sum([ord(i) for i in str(self.transactions)])
         cur_hash = (prev + trans) - self.nonce
         return cur_hash
 
     def solve_puzzle(self):
         '''for the time being just pretend to solve a problem'''
         print('beginning to generate new block...')
-        time.sleep(np.random.randint(10))
+        value        = 0
+        while value != self.target and self.interrupt == False:
+            time.sleep(np.random.randint(10))
+            value += 1
         print('created new block!')
 
         return hash(str(self.prev_hash) + str(self.transactions) + str(self.nonce))
@@ -72,10 +79,28 @@ class Network:
     def __init__(self):
         self.newest_block           = None
         self.requested_transactions = []
+        self.connected_miners       = []
+        self.verbose                = 2
+        self.list_of_mined_blocks   = []
+
+    def establish_connection(self, miner):
+        print('\n Received new connection from ' + miner.name + '\n')
+        self.connected_miners.append(miner)
+
+    def receive_new_block(self, block, miner_name):
+        print("Network received a new block! Broadcasting to all miners on network")
+        self.newest_block = block
+        self.list_of_mined_blocks.append(block)
+        for miner in self.connected_miners:
+            if miner.name != miner_name:
+                miner.receive_new_block(self)
+
+
 
 class Transaction:
     def __init__(self, transactions):
         self.transactions  = transactions
+        self.verbose       = 2
 
     def receive_request(self, trade):
         self.transactions.append(trade)
@@ -112,7 +137,8 @@ class Person:
 
 class Exchange:
     def __init__(self, name, network_instance):
-        self.name     = name
+        self.name                  = name
+        self.verbose               = 2
         self.connection_to_network = network_instance
 
     def receive_request(self, trade_instance):
@@ -121,7 +147,8 @@ class Exchange:
             trade_instance.sender.coin_possessed -= trade_instance.amount
             return True
         else:
-            print('Detected fraudulent transaction!')
+            if self.verbose >= 2:
+                print('Detected fraudulent transaction!')
             return False
 
     def broadcast_request(self, trade):
@@ -129,30 +156,82 @@ class Exchange:
 
 
 class Miner:
-    def __init__(self, network_connection):
+    def __init__(self, name, network_connection):
+        self.name             = name
         self.num_blocks_mined = 0
         self.mined_blocks     = []
         self.network          = network_connection
+        self.network.establish_connection(self)
         self.pending_trades   = None
-        self.start_mining()
         self.newest_block     = self.network.newest_block
+        self.interrupt        = False
+        self.newest_block     = network.newest_block
+        self.verbose          = 2
+
 
     def start_mining(self):
         #first get transactions from the network
         self.pending_trades = self.network.requested_transactions
+        last_block          = self.network.newest_block
+        value               = 0
+        target              = 10
+        if self.verbose >= 2:
+            print(self.name + ': Begin mining for new block')
+
+        #keep working until you receive interrupt or until you find a new block
+        while self.interrupt == False and value != target:
+            time.sleep(np.random.randint(5))
+            value += 1
+
+        #if you found a new block, broadcast it and start working on next block
+        if value == target:
+            print(self.name + ': I found a new block! Letting network know about it.')
+            new_block = Block(hash(str(last_block)), self.pending_trades)
+            self.broadcast_new_block(new_block)
+            self.start_mining()
+
+        #if someone else found the block before you, update your state, then start working on next block.
+        elif self.interrupt == True and value != target:
+            print(self.name + ': Someone else found a block. Starting over.')
+            #updating state is handled at beginning of function, so just make recursive call
+            self.interrupt = False
+            self.start_mining()
+
+
+
+    def broadcast_new_block(self, block):
+        self.network.receive_new_block(block, self.name)
+
+    def receive_new_block(self, block):
+        self.interrupt = True
 
 
 
 
-    def find_block(self):
-        new_block = Block(str(hash('genesis_block')), str(self.transactions_list))
+
+
+
+
+
 
 
 if __name__ == '__main__':
+    print('creating network')
     network              = Network()
     genesis_block        = Block('genesis_block', 'first_transaction')
     network.newest_block = genesis_block
 
+    print('creating exchange')
     demo_exchange        = Exchange('demo_exchange', network)
+    demo_exchange.verbose = 1
+
+    print('creating people')
     people               = People(demo_exchange)
-    people.start_generating_transactions()
+    people.verbose       = 1
+    transactions_thread  = threading.Thread(target=people.start_generating_transactions)
+    transactions_thread.start()
+
+    print('creating miner')
+    miner_0              = Miner('miner_0', network)
+    mining_thread        = threading.Thread(target=miner_0.start_mining())
+    mining_thread.start()
