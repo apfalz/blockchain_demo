a0c677ca1662ed370b7c0dfcd8d89f0f568963ed
Andrew Pfalz <apfalz@cct.lsu.edu>
2018-06-19 11:23:02 -0500

    add network class

--- bitcoin_lib.py
+++ bitcoin_lib.py
@@ -5,11 +5,29 @@ from Crypto import Random
 from Crypto.PublicKey import RSA
 
 class People:
-    def __init__(self):
-        self.people = {}
+    def __init__(self, exchange_instance):
+        self.people   = {}
+        self.exchange = exchange_instance
+        self.names    = ['Alice', 'Bob', 'Carly', 'Dave', 'Emma', 'Frank', 'Gwen', 'Henry', 'Iggy', 'James', 'Karen', 'Larry']
+        #add people to the table
+        for i, name in enumerate(self.names):
+            self.add_person(Person(np.random.randint(1, 20), i, name))
 
     def add_person(self, person_instance):
         self.people[person_instance.name] = person_instance
+
+    def start_generating_transactions(self):
+        for i in range(200):
+            sender    = self.people[self.names[np.random.randint(len(self.names))]]
+            recipient = self.people[self.names[np.random.randint(len(self.names))]]#for now allow sending coin to yourself
+            amount    = np.random.randint(1, 20)
+            posessed  = '/' + str(sender.coin_possessed)
+            trade     = Trade(sender, recipient, amount)
+            print(sender.name + ' wants to send ' + str(amount) + posessed + ' to ' + recipient.name)
+            self.exchange.receive_request(trade)
+
+
+            time.sleep(np.random.randint(2))
 
 class Block:
     def __init__(self, prev_hash, transaction_list):
@@ -35,6 +53,7 @@ class Block:
         print('beginning to generate new block...')
         time.sleep(np.random.randint(10))
         print('created new block!')
+
         return hash(str(self.prev_hash) + str(self.transactions) + str(self.nonce))
 
 
@@ -49,7 +68,10 @@ class Block:
         return cur_hash
 
 
-
+class Network:
+    def __init__(self):
+        self.newest_block           = None
+        self.requested_transactions = []
 
 class Transaction:
     def __init__(self, transactions):
@@ -83,39 +105,43 @@ class Person:
 
     def request_trade(self, exchange, recipient, amount):
         self.coin_possessed -= amount
-        result               = exchange.receive_request(Trade(self,
-                                                              recipient,
-                                                              amount))
-        return result
+
 
     def receive_trade(self, amount):
         self.coin_possessed += amount
 
 class Exchange:
-    def __init__(self, name):
+    def __init__(self, name, network_instance):
         self.name     = name
-        self.requests = []
+        self.connection_to_network = network_instance
 
     def receive_request(self, trade_instance):
         if trade_instance.amount <= trade_instance.sender.coin_possessed:
-            self.requests.append(trade_instance)
+            self.broadcast_request(trade_instance)
+            trade_instance.sender.coin_possessed -= trade_instance.amount
             return True
         else:
             print('Detected fraudulent transaction!')
             return False
 
-    def broadcast_requests(self):
-        return
+    def broadcast_request(self, trade):
+        self.connection_to_network.requested_transactions.append(trade)
+
 
 class Miner:
-    def __init__(self):
+    def __init__(self, network_connection):
         self.num_blocks_mined = 0
         self.mined_blocks     = []
+        self.network          = network_connection
+        self.pending_trades   = None
         self.start_mining()
+        self.newest_block     = self.network.newest_block
 
     def start_mining(self):
-        with open('transactions.txt', 'r') as f:
-            self.transactions_list = f.readlines()
+        #first get transactions from the network
+        self.pending_trades = self.network.requested_transactions
+
+
 
 
     def find_block(self):
@@ -123,7 +149,10 @@ class Miner:
 
 
 if __name__ == '__main__':
-    first_hash        = hash('arbitrary string serving as has for genesis block')
-    genesis_block     = Block(str(first_hash), 'first_transaction')
+    network              = Network()
+    genesis_block        = Block('genesis_block', 'first_transaction')
+    network.newest_block = genesis_block
 
-    genesis_block.solve_puzzle()
+    demo_exchange        = Exchange('demo_exchange', network)
+    people               = People(demo_exchange)
+    people.start_generating_transactions()
